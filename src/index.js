0b76161d73e5075b75c067e36c67ac29a74f69cf
Dima Dubnovitsky <dimadubnovitsky@gmail.com>
2019-03-24 20:58:28 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  for (let i = 0; i < str.length; i++) {
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      let inLine = bracketsConfig[j].join("");
+      str = str.replace(inLine, "");
+    }
+  }
+
+  if (str.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
